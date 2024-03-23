bafc2c32aa1dcd63bd502b43949f8de2b9a037bd
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-03-23 22:56:28 +0900

    fix: filetypeいらないので消し

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -1,12 +1,5 @@
-import { fileTypeFromBuffer } from 'file-type';
-
 const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
 
 export async function isSupportedImage(image: File): Promise<boolean> {
-  const fileType = await fileTypeFromBuffer(await image.arrayBuffer());
-  if (SUPPORTED_MIME_TYPE_LIST.includes(fileType?.mime ?? '')) {
-    return true;
-  }
-
-  return false;
+  return SUPPORTED_MIME_TYPE_LIST.includes(image.type);
 }

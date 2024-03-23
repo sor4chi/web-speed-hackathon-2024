ccc59bf800d52bbe8a9323cabeb07cedf13927d6
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-03-24 08:08:35 +0900

    feat: use webp, good bye jxl

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,8 +1,5 @@
 /// <reference types="@types/serviceworker" />
 
-import { transformJpegXLToBmp } from './transformJpegXLToBmp';
-import { zstdFetch as fetch } from './zstdFetch';
-
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
 });
@@ -10,23 +7,3 @@ self.addEventListener('install', (ev: ExtendableEvent) => {
 self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  if (!ev.request.url.includes('jxl')) {
-    return;
-  }
-
-  return ev.respondWith(onFetch(ev.request));
-});
-
-async function onFetch(request: Request): Promise<Response> {
-  // サーバーの負荷を分散するために Jitter 処理をいれる
-
-  const res = await fetch(request);
-
-  if (res.headers.get('Content-Type') === 'image/jxl') {
-    return transformJpegXLToBmp(res);
-  } else {
-    return res;
-  }
-}

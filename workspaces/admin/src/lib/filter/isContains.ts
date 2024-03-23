5c709f06cd591f999fdc39ac3436433ca2ab78a3
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-03-23 23:11:04 +0900

    fix: isContainsが正しく動作するように修正

--- workspaces/admin/src/lib/filter/isContains.ts
+++ workspaces/admin/src/lib/filter/isContains.ts
@@ -5,11 +5,11 @@ type Params = {
 
 // ひらがな・カタカナ・半角・全角を区別せずに文字列が含まれているかを調べる
 export function isContains({ query, target }: Params): boolean {
-  // ひらがな・カタカナ・半角・全角をすべてひらがなに変換する
-  const normalizedQuery = query.normalize('NFKC').replace(/[ぁ-んァ-ンｧ-ﾝ]/g, (char) => {
+  // カタカナ・半角・全角をすべてひらがなに変換する
+  const normalizedQuery = query.normalize('NFKC').replace(/[ァ-ンｧ-ﾝ]/g, (char) => {
     return String.fromCharCode(char.charCodeAt(0) - 0x60);
   });
-  const normalizedTarget = target.normalize('NFKC').replace(/[ぁ-んァ-ンｧ-ﾝ]/g, (char) => {
+  const normalizedTarget = target.normalize('NFKC').replace(/[ァ-ンｧ-ﾝ]/g, (char) => {
     return String.fromCharCode(char.charCodeAt(0) - 0x60);
   });
 

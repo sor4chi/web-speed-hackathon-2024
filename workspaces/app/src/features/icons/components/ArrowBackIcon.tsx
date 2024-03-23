da82e8fd6904dc50c533d2e89d43ceef71588805
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-03-24 08:53:15 +0900

    perf: muiを削除

---
+++ workspaces/app/src/features/icons/components/ArrowBackIcon.tsx
@@ -0,0 +1,12 @@
+export const ArrowBackIcon = () => (
+  <svg
+    aria-hidden="true"
+    className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+    data-testid="ArrowBackIcon"
+    focusable="false"
+    style={{ fill: '#202020', height: 32, width: 32 }}
+    viewBox="0 0 24 24"
+  >
+    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"></path>
+  </svg>
+);
